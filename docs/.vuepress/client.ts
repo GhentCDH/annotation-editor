ae110293a2b13d05827d8158633a5bbe1130639a
Bo Vandersteene <bo@firepeak.tech>
2026-08-20 11:39:44 +0200

    fix: docs editors

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,5 +1,8 @@
 import { defineClientConfig } from 'vuepress/client';
 
 import './styles/app.css';
+import { configureApi } from '../../packages/annotation-vue/src/lib/service/useApi';
+import axios from 'axios';
 
+configureApi(axios);
 export default defineClientConfig({});
