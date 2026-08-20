84c95ecce49a0ed88dfa1bcf8743c3cb1f9500aa
Bo Vandersteene <bo@firepeak.tech>
2026-08-20 15:25:31 +0200

    fix: load only when div is on the screen

--- packages/annotation-editor/src/lib/components/SourceEdit.vue
+++ packages/annotation-editor/src/lib/components/SourceEdit.vue
@@ -1,20 +1,14 @@
 <template>
-  <SourceNavbar
-    v-bind="properties"
-    @create-annotation="createAnnotation"
-  />
+  <SourceNavbar v-bind="properties" @create-annotation="createAnnotation" />
   <div class="overflow-y-auto flex-1 min-h-0">
-    <div
-      :id="textUuid"
-      :dir="source.content.textDirection"
-    />
+    <div :id="textUuid" :dir="source.content.textDirection" ref="mainEl" />
   </div>
 </template>
 <script lang="ts" setup>
 import { v4 as uuid } from 'uuid';
 import { type AnnotatedText } from '@ghentcdh/annotated-text';
 import { type W3CAnnotation } from '@ghentcdh/w3c-utils';
-import { onMounted, onUnmounted, watch } from 'vue';
+import { onMounted, onUnmounted, ref, watch } from 'vue';
 import { SourceEditProperties } from './SourceEdit.properties';
 import SourceNavbar from './SourceNavbar.vue';
 import { useEditorState } from '../composables/useEditorState';
@@ -26,6 +20,9 @@ const { config, sendAnnotationEvent, editorState, utils } = useEditorState();
 const textUuid = `text-content-${uuid()}`;
 
 let textAnnotation: AnnotatedText<W3CAnnotation> | undefined = undefined;
+
+const mainEl = ref(null);
+let observer: IntersectionObserver | null = null;
 
 watch(
   () => properties.source,
@@ -51,6 +48,19 @@ watch(
 );
 
 onMounted(() => {
+  observer = new IntersectionObserver(([entry]) => {
+    if (entry.isIntersecting) {
+      drawTextAnnotation();
+      observer?.disconnect(); // Stop watching once visible, if desired
+    }
+  });
+
+  if (mainEl.value) {
+    observer.observe(mainEl.value);
+  }
+});
+
+const drawTextAnnotation = () => {
   textAnnotation = config.annotation
     .createAnnotatedText(textUuid, properties.source)
     .setTagLabelFn((annotation: W3CAnnotation) => {
@@ -66,10 +76,11 @@ onMounted(() => {
     })
     .setText(properties.source?.content.text ?? '')
     .setAnnotations(properties.annotations ?? []);
-});
+};
 
 onUnmounted(() => {
   textAnnotation?.destroy();
+  if (observer) observer.disconnect();
 });
 
 const createAnnotation = (annotationType: string) => {
